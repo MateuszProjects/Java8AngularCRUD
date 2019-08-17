5f46e5a4a5935d806d6fc4d7b1c2dd190db1a62a
mateusz <dorobisz.mateusz@o2.pl>
2019-08-17 16:32:58 +0200

    Commit#3

---
+++ Backend/java8Backend/src/main/java/com/example/demo/Java8BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8BackendApplication.class, args);
+	}
+
+}
